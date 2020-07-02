7fe1ce085e4a6ff1e10a28e9be38612db80e6a3e
Cloud User <centos@ip-172-31-64-234.ec2.internal>
2020-07-02 02:24:08 +0000

    add check

---
+++ vars/golangCheck.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+// check bach syntax
+def call(){
+
+    sh 'go get -u golang.org/x/lint/golint'
+    sh 'go list -f {{.Target}} golang.org/x/lint/golint'
+    sh 'export PATH="$PATH:/go/bin/"'
+    sh 'golint  \${WORKSPACE}/'
+
+
+}
