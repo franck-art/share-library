08b557c3b089b5d8b25a0f2507de797839d9efda
Cloud User <centos@ip-172-31-72-152.ec2.internal>
2020-07-04 13:28:22 +0000

    update golang check

--- vars/golangCheck.groovy
+++ vars/golangCheck.groovy
@@ -2,10 +2,9 @@
 
 // check bach syntax
 def call(){
-
+    sh 'sudo yum install golang -y'
     sh 'go get -u golang.org/x/lint/golint'
-    sh 'go list -f {{.Target}} golang.org/x/lint/golint'
-    sh 'export PATH="$PATH:/go/bin/"'
+    sh 'export PATH="$PATH:/home/centos/go/bin/"'
     sh 'golint  \${WORKSPACE}/fake-backend/main.go'
     sh 'golint  \${WORKSPACE}/fake-backend/config.go'
 
