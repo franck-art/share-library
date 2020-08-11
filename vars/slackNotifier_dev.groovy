463438bdc1372912982ed3f985de6067fdcd19b8
franck-art <58075364+franck-art@users.noreply.github.com>
2020-08-11 18:55:54 +0200

    Update slackNotifier_dev.groovy

--- vars/slackNotifier_dev.groovy
+++ vars/slackNotifier_dev.groovy
@@ -1,5 +1,5 @@
 def call(String buildResult) {
   if( buildResult == "FAILURE" ) { 
     slackSend color: "danger", message: "BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed ! more info ${env.BUILD_URL}"
-
+  }
 }
