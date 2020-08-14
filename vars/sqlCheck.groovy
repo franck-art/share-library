f707af37e1f42dc0bf12b2927f080681c86058ae
franck-art <58075364+franck-art@users.noreply.github.com>
2020-08-14 18:40:53 +0200

    Update sqlCheck.groovy

--- vars/sqlCheck.groovy
+++ vars/sqlCheck.groovy
@@ -3,8 +3,8 @@
 // check bach syntax
 def call(){
     sh 'sudo curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -'
-    sh 'sudo yum install nodejs'
-    sh 'sudo npm install -g sql-lint'
+    sh 'sudo yum install nodejs -y'
+    sh 'sudo npm install -g sql-lint -y'
     sh 'sql-lint  \${WORKSPACE}/database/crud_flask.sql'
 
 
