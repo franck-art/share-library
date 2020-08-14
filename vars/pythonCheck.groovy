c57b8ccdaeead3440da781d368ac78f65ca5cc35
franck-art <58075364+franck-art@users.noreply.github.com>
2020-08-14 18:40:22 +0200

    Update pythonCheck.groovy

--- vars/pythonCheck.groovy
+++ vars/pythonCheck.groovy
@@ -3,10 +3,10 @@
 // check python syntax
 def call(){
   
-   sh 'sudo yum install epel-release'
-   sh 'sudo yum install python-pip'
+   sh 'sudo yum install epel-release -y'
+   sh 'sudo yum install python-pip -y'
    sh 'pip --version'
-   sh 'pip install pylint'
+   sh 'pip install pylint -y'
    sh 'pylint  \${WORKSPACE}/source_code/module/database.py'
 
 }
